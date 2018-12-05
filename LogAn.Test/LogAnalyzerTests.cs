17a9175036d8dc7890b9cd35dc652ab96ab286f3
Weidaicheng <wdcdavyc@outlook.com>
2018-12-05 15:41:50 +0800

    Add more tests

--- LogAn.Test/LogAnalyzerTests.cs
+++ LogAn.Test/LogAnalyzerTests.cs
@@ -4,19 +4,36 @@ namespace LogAn.Tests
 {
     public class LogAnalyzerTests
     {
+        private LogAnalyzer _analyzer;
+
         [SetUp]
         public void Setup()
         {
+            _analyzer = new LogAnalyzer();
         }
 
         [Test]
-        public void IsValidFileName_BadExtension_ReturnFalse()
+        public void IsValidFileName_BadExtension_ReturnsFalse()
         {
-            LogAnalyzer analyzer = new LogAnalyzer();
-
-            bool result = analyzer.IsValidLogFileName("filewithbadextension.foo");
+            bool result = _analyzer.IsValidLogFileName("filewithbadextension.foo");
 
             Assert.False(result);
+        }
+
+        [Test]
+        public void IsValidFileName_GoodExtensionLowercase_ReturnsTrue()
+        {
+            bool result = _analyzer.IsValidLogFileName("filewithgoodextension.slf");
+
+            Assert.True(result);
+        }
+
+        [Test]
+        public void IsValidFileName_GoodExtensionUppercase_ReturnsTrue()
+        {
+            bool result = _analyzer.IsValidLogFileName("filewithgoodextension.SLF");
+
+            Assert.True(result);
         }
     }
 }
